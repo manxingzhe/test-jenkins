fca8247d73d8eb213a7816195e288803ee578253
yangbensheng <244122198@qq.com>
2019-11-24 15:37:43 +0800

    修改原因：测试jenkins

---
+++ src/main/java/ybs/testjenkins/controller/HelloController.java
@@ -0,0 +1,15 @@
+package ybs.testjenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String testHello(ModelMap modelMap){
+        modelMap.put("hello","hello world");
+        return "index";
+    }
+}
---
+++ src/test/java/ybs/testjenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ybs.testjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
