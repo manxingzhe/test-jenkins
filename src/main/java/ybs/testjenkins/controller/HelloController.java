0bb574228e38a3ae1c7d8abbd1136b236db9188f
yangbensheng <244122198@qq.com>
2019-12-01 15:22:35 +0800

    修改原因：测试jenkins07

--- src/main/java/ybs/testjenkins/controller/HelloController.java
+++ src/main/java/ybs/testjenkins/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @RequestMapping("/index")
     public String testHello(ModelMap modelMap){
-        modelMap.put("hello","hello world66");
+        modelMap.put("hello","hello world77");
         return "index";
     }
 }
