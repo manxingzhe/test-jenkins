faa78db4c8aa57ba05a70633348e2fa54159cfd5
yangbensheng <244122198@qq.com>
2019-12-01 18:15:07 +0800

    修改原因：测试jenkins103

--- src/main/java/ybs/testjenkins/controller/HelloController.java
+++ src/main/java/ybs/testjenkins/controller/HelloController.java
@@ -3,11 +3,13 @@ package ybs.testjenkins.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String testHello(ModelMap modelMap){
         modelMap.put("hello","hello world 101");
         return "index";
